18d6b24a3e5c5b4571402f6689786a40bf0de7b5
ChoIntelligence <132897663+ChoIntelligence@users.noreply.github.com>
2023-05-09 14:19:17 +0900

    Create 10810.py

---
+++ 10810.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+a = {}
+for i in range(n) :
+    a[i] = 0
+for i in range(m) :
+    x ,y, z = map(int, input().split())
+    for j in range(x - 1, y) :
+        a[j] = z
+for i in range(n) :
+    print(a[i], end = " ")
