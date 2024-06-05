c927da37fea9ec4d6684028294ecf0c96ab2ab9b
ChoIntelligence <132897663+ChoIntelligence@users.noreply.github.com>
2024-06-05 19:23:34 +0900

    Update macro.py

--- macro.py
+++ macro.py
@@ -11,55 +11,55 @@ time.sleep(5)
 for i in range(10000):
     time.sleep(2)
 
-    pyautogui.press('tab'); time.sleep(0.05)
-    pyautogui.press('tab'); time.sleep(0.05)
-    pyautogui.press('tab'); time.sleep(0.05)
+    pyautogui.press('tab'); time.sleep(0.2)
+    pyautogui.press('tab'); time.sleep(0.2)
+    pyautogui.press('tab'); time.sleep(0.2)
 
-    pyautogui.press('down'); time.sleep(0.05)
-
-    rand = random.randint(0, 100)
-
-    if rand % 2 == 0:
-        pyautogui.press('up'); time.sleep(0.05)
-
-    pyautogui.press('tab'); time.sleep(0.05)
-
-    pyautogui.press('down'); time.sleep(0.05)
+    pyautogui.press('down'); time.sleep(0.2)
 
     rand = random.randint(0, 100)
 
     if rand % 2 == 0:
-        pyautogui.press('up'); time.sleep(0.05)
+        pyautogui.press('up'); time.sleep(0.2)
+
+    pyautogui.press('tab'); time.sleep(0.2)
+
+    pyautogui.press('down'); time.sleep(0.2)
+
+    rand = random.randint(0, 100)
+
+    if rand % 2 == 0:
+        pyautogui.press('up'); time.sleep(0.2)
 
     for _ in range(21):
-        pyautogui.press('tab'); time.sleep(0.05)
+        pyautogui.press('tab'); time.sleep(0.2)
 
-        pyautogui.press('space'); time.sleep(0.05)
+        pyautogui.press('space'); time.sleep(0.2)
 
         rand = random.randint(0, 4)
 
         for _ in range(rand):
-            pyautogui.press('down'); time.sleep(0.05)
+            pyautogui.press('down'); time.sleep(0.2)
 
-        pyautogui.press('tab'); time.sleep(0.05)
+        pyautogui.press('tab'); time.sleep(0.2)
 
-        pyautogui.press('space'); time.sleep(0.05)
+        pyautogui.press('space'); time.sleep(0.2)
 
         for _ in range(4 - rand):
-            pyautogui.press('down'); time.sleep(0.05)
+            pyautogui.press('down'); time.sleep(0.2)
 
-    pyautogui.press('tab'); time.sleep(0.05)
+    pyautogui.press('tab'); time.sleep(0.2)
 
     front = random.randint(0, 9999)
     back = random.randint(0, 9999)
 
     phone_number = f"010-{front:04d}-{back:04d}"
-    pyautogui.typewrite(phone_number); time.sleep(0.05)
+    pyautogui.typewrite(phone_number); time.sleep(0.2)
 
-    pyautogui.press('tab'); time.sleep(0.05)
+    pyautogui.press('tab'); time.sleep(0.2)
 
     pyautogui.press('enter'); time.sleep(2)
 
-    pyautogui.press('tab'); time.sleep(0.05)
+    pyautogui.press('tab'); time.sleep(0.2)
 
-    pyautogui.press('enter'); time.sleep(0.05)
+    pyautogui.press('enter'); time.sleep(0.2)
